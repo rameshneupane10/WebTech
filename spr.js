706d8a4eed21447664169ff5830574d3720392b3
rameshneupane10 <rameshnjr10@gmail.com>
2023-09-19 23:51:03 +0545

    changes made

---
+++ spr.js
@@ -0,0 +1,25 @@
+function Scissor()
+{
+    document.getElementById("result").innerHTML="Scissor called"; 
+}
+function Paper()
+{
+    document.getElementById("result").innerHTML="Paper called";  
+}
+function Rock()
+{
+    var a= Math.random*("Scissors","paper","rock");
+    a=parseInt(a);
+    document.getElementById("result").values =a;
+    display();
+ 
+}
+function display()
+{
+    if(a == "Scissors")
+    {
+        document.getElementById("result").innerHTML="ok";  
+    }
+
+}
+
