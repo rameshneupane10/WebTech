21d6e87216fce5ca38a522b29fd0b94338ad6863
rameshneupane10 <rameshnjr10@gmail.com>
2023-09-22 13:37:33 +0545

    changes made

--- spr.js
+++ spr.js
@@ -1,31 +1,60 @@
 function Scissor() {
+    var pick =0;
     var arr = ["Scissor", "Paper", "Rock"];
     var a = Math.floor(Math.random() * 3);
     document.getElementById("result").innerHTML = arr[a];
     display(a);
+    if(pick==a)
+    {
+        document.getElementById("winner").innerHTML = "DRAW";
+    }
+    else if(a==1)
+    {
+        document.getElementById("winner").innerHTML = "YOU WON!!!";
+    }
+    else{
+        document.getElementById("winner").innerHTML = "YOU LOSE!!!";
+    }
 }
 function Paper() {
+    var pick =1;
     var arr = ["Scissor", "Paper", "Rock"];
     var a = Math.floor(Math.random() * 3);
     document.getElementById("result").innerHTML = arr[a];
     display(a);
+    if(pick==a)
+    {
+        document.getElementById("winner").innerHTML = "DRAW";
+    }
+    else if(a==2)
+    {
+        document.getElementById("winner").innerHTML = "YOU WON!!!";
+    }
+    else{
+        document.getElementById("winner").innerHTML = "YOU LOSE!!!";
+    }
 }
 function Rock() {
-    var arr = ["Scissor", "Paper", "Rock"];
+    var pick =2;
+    var arr = ["Scissor", "Paper", "Rock"];                     // scissor=0 , paper =1  ,rock =2
     var a = Math.floor(Math.random() * 3);
     document.getElementById("result").innerHTML = arr[a];
     display(a);
+    if(pick==a)
+    {
+        document.getElementById("winner").innerHTML = "DRAW";
+    }
+    else if(a==0)
+    {
+        document.getElementById("winner").innerHTML = "YOU WON!!!";
+    }
+    else{
+        document.getElementById("winner").innerHTML = "YOU LOSE!!!";
+    }
 }
-function Opponent()
-{
-    var arr = ["Scissor", "Paper", "Rock"];
-    var a = Math.floor(Math.random() * 3);
-    document.getElementById("result").innerHTML = arr[a];
-    display(a);
-}
-function display(a) {
-    if (a === 0) {
-        document.getElementById("result").innerHTML = "Opponent chose Scissor";
+function display(a) {                      
+        if (a === 0) {
+            document.getElementById("result").innerHTML = "Opponent chose Scissor";
     } else if (a === 1) {
         document.getElementById("result").innerHTML = "Opponent chose Paper";
     } else if (a === 2) {
