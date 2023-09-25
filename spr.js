0f2fa0fdce4ea31d4cdfe2ae8ab4a0a1d401325c
rameshneupane10 <rameshnjr10@gmail.com>
2023-09-25 10:04:14 +0545

    changes made

--- spr.js
+++ spr.js
@@ -1,11 +1,14 @@
 function Scissor() {
+   
     var pick =0;
     var arr = ["Scissor", "Paper", "Rock"];
     var a = Math.floor(Math.random() * 3);
     document.getElementById("result").innerHTML = arr[a];
     display(a);
+    document.getElementById('icon1').style.display = "block";
     if(pick==a)
     {
+        document.getElementById('icon1').style.display = "block";
         document.getElementById("winner").innerHTML = "DRAW";
     }
     else if(a==1)
@@ -14,8 +17,10 @@ function Scissor() {
     }
     else{
         document.getElementById("winner").innerHTML = "YOU LOSE!!!";
-    }
+    }     
+                             
 }
+
 function Paper() {
     var pick =1;
     var arr = ["Scissor", "Paper", "Rock"];
@@ -25,6 +30,7 @@ function Paper() {
     if(pick==a)
     {
         document.getElementById("winner").innerHTML = "DRAW";
+       
     }
     else if(a==2)
     {
@@ -33,6 +39,7 @@ function Paper() {
     else{
         document.getElementById("winner").innerHTML = "YOU LOSE!!!";
     }
+    document.getElementById('icon2').style.display = "block";
 }
 function Rock() {
     var pick =2;
@@ -51,13 +58,18 @@ function Rock() {
     else{
         document.getElementById("winner").innerHTML = "YOU LOSE!!!";
     }
+    document.getElementById('icon3').style.display = "block";
 }
 function display(a) {                      
         if (a === 0) {
             document.getElementById("result").innerHTML = "Opponent chose Scissor";
+            document.getElementById('icon4').style.display = "block";
+
     } else if (a === 1) {
         document.getElementById("result").innerHTML = "Opponent chose Paper";
+        document.getElementById('icon5').style.display = "block";
     } else if (a === 2) {
         document.getElementById("result").innerHTML = "Opponent chose Rock";
+        document.getElementById('icon6').style.display = "block";
     }
 }
